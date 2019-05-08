dbd7574067d775f6652aed6009929714d1d793b4
DanielKanyo <danielkanyog@gmail.com>
2019-05-08 14:25:48 +0200

    Dependencie update, panel action container in CompanySummary

--- src/Components/Orders/CompanySummary.jsx
+++ src/Components/Orders/CompanySummary.jsx
@@ -4,12 +4,17 @@ import { withStyles } from '@material-ui/core/styles'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
 import Typography from '@material-ui/core/Typography'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 import Avatar from '@material-ui/core/Avatar'
+import Divider from '@material-ui/core/Divider'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/DeleteOutline'
+import EditIcon from '@material-ui/icons/EditOutlined'
 
 const styles = theme => ({
   heading: {
@@ -18,6 +23,9 @@ const styles = theme => ({
   },
   expanded: {
     margin: '8px 0'
+  },
+  action: {
+    padding: 8
   },
   smallAvatar: {
     width: 22,
@@ -42,6 +50,15 @@ const CompanySummary = ({ classes, auth, company }) => {
         <ExpansionPanelDetails>
           <Typography>{company.description}</Typography>
         </ExpansionPanelDetails>
+        <Divider />
+        <ExpansionPanelActions className={classes.action}>
+          <IconButton aria-label="Edit">
+            <EditIcon fontSize="small" />
+          </IconButton>
+          <IconButton aria-label="Delete">
+            <DeleteIcon fontSize="small" />
+          </IconButton>
+        </ExpansionPanelActions>
       </ExpansionPanel>
     </React.Fragment>
   )
